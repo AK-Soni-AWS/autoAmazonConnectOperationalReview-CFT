f29847c5a20115718b8f973c7a5e23f01ce4d556
AK Soni <achlesh@amazon.com>
2026-05-03 16:24:12 -0500

    licence + remove cft param

--- scripts/update_cft_lambda.py
+++ scripts/update_cft_lambda.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# Copyright Amazon.com, Inc. or its affiliates. This material is AWS Content under the AWS Enterprise Agreement
+# or AWS Customer Agreement (as applicable) and is provided under the AWS Intellectual Property License.
+
 """
 Update embedded Lambda function code in CloudFormation template.
 Supports both same-repo and remote-repo scenarios.
